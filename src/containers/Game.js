97e6c4efc991fc73395b4ba43ab2bb171744d9a5
a3nath <amarn.sks@gmail.com>
2021-08-26 12:36:25 -0400

    game artist form

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -3,21 +3,53 @@ import Button from  '../components/UI/Button/Button';
 import Input from '../components/Input/Input';
 
 const Game = props => {
-    
-    const inputElement = props => {
-        <input 
-            type='text'
-            placeholder='Artist Name'
-            name='artist'
-            id='artist'
-            onChange={props.changed}
-            value={props.value}
-        />
+
+    const [artistForm, setArtistForm] = useState({
+            value:'',
+            elementType:'input',
+            elementConfig:{
+                type:'text',
+                placeholder:'Artist name'
+            },
+            validation:{
+                required:true
+            },
+            valid:false,
+            touched:false
+    })
+
+    //checks if form valid
+    const [formValid, setFormValid] = useState(false);
+
+    const validHandler = artist => {
+        //if artist exists
+        //api call
     }
 
-    const artistHandler = event => {
+    //form input handler, triggers when submit form
+    const inputHandler = event => {
         event.preventDefault;
-
+        const newForm = {...artistForm};
+        newForm.value = event.target.value;
+        newForm.touched = true;
+        //check if artist exists
+        // newForm.valid = validHandler()
+        setArtistForm(newForm)
+        //check form valid
+        // setFormValid(newForm.valid)
+    }
+    
+    const inputElement = () => {
+        <input 
+            inputType={artistForm.elementConfig.type}
+            placeholder={artistForm.elementConfig.placeholder}
+            name='artist'
+            id='artist'
+            changed={(event) => inputHandler(event)}
+            value={artistForm.value}
+            valid={artistForm.valid}
+            touched={artistForm.touched}
+        />
     }
 
     //form valid
@@ -30,7 +62,7 @@ const Game = props => {
         <div>
             Hello, thsis is Game Page
             <div>
-                <form onSubmit={artistHandler}>
+                <form onSubmit={inputHandler}>
                     <label for='artist'>Enter artist Name</label>
                     {inputElement}
                     <Button disabled={!formValid} BtnType='Success'>Submit</Button>
