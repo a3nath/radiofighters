e5597bf5a6b567ab87cb31190849dc9a6a5f8aa1
a3nath <amarn.sks@gmail.com>
2021-08-30 15:03:40 -0400

    promise all

--- src/store/actionCreators/index.js
+++ src/store/actionCreators/index.js
@@ -2,8 +2,10 @@ import * as actionTypes from '../actionTypes';
 
 //API Key
 const musicApp = {};
-musicApp.audioDBBaseurl = "https://www.theaudiodb.com/api/v1/json";
+musicApp.audioDBBaseurl = "https://www.theaudiodb.com/api/v1/json"; 
 musicApp.audioDBAPI = "523532";
+const searchArt = 'search.php?s='
+const searchAlbum = 'searchalbum.php?s='
 
 export const loading = () => {
     return {type: actionTypes.LOADING}
@@ -27,32 +29,49 @@ export const error = err => {
 
 
 export const addArtistThunk = (artist) => {
-    const url = `${musicApp.audioDBBaseurl}/${musicApp.audioDBAPI}/search.php?s=${artist}`
+    //artist general info
+    // const url = `${musicApp.audioDBBaseurl}/${musicApp.audioDBAPI}/search.php?s=${artist}`
+    //discography
+    const urlArt =  `${musicApp.audioDBBaseurl}/${musicApp.audioDBAPI}/${searchArt}${artist}`
+    const urlAlbum =  `${musicApp.audioDBBaseurl}/${musicApp.audioDBAPI}/${searchAlbum}${artist}`
     return (dispatch) => {
-        return(fetch(url)
-            .then(response => {
-                    if (response.ok){
-                        return response                    
-                    }//get response but not ok
-                    else {
-                        const error = new Error(`Error ${response.status}: ${response.statusText}`)
-                        error.response = response;
-                        throw error;
-                    }
-                },
-                //if you dont get response
-                error => {
-                        const errMess = new Error(error.message);
-                        throw errMess;
-                    }
+        Promise.all([fetch(urlArt),fetch(urlAlbum)])
+            .then(responses => {
+                    return Promise.all(responses.map(res => res.json()))})
+            .then(
+                data => console.log(data)
             )
-            .then(res => res.json())// dispatch(addArtist(res.json()))
-            //artist object
-            .then(value => dispatch(addArtist([value.artists[0]]))
-                // dispatch(addArtist(value.artists[0].strArtist))
-            )
-            .catch(err => dispatch(error(err)))
-            )
+            .catch(err => dispatch(error(err)));
         }
 }
+            
+        
 
+        
+        
+//         return(fetch(url)
+//             .then(response => {
+//                     if (response.ok){
+//                         return response                    
+//                     }//get response but not ok
+//                     else {
+//                         const error = new Error(`Error ${response.status}: ${response.statusText}`)
+//                         error.response = response;
+//                         throw error;
+//                     }
+//                 },
+//                 //if you dont get response
+//                 error => {
+//                         const errMess = new Error(error.message);
+//                         throw errMess;
+//                     }
+//             )
+//             .then(res => res.json())// dispatch(addArtist(res.json()))
+//             //artist object
+//             .then(value => console.log(value)
+//                 // dispatch(addArtist([value.artists[0]]))
+//             )
+//             .catch(err => dispatch(error(err)))
+//             )
+//         }
+// }
