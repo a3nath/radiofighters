d7ef83cb8b7e82fddc8ea0241649a7f79ff413f1
a3nath <amarn.sks@gmail.com>
2021-09-29 15:28:26 -0400

    media query1 game

--- src/containers/Checkout/CheckoutSummary.js/CheckoutSummary.js
+++ src/containers/Checkout/CheckoutSummary.js/CheckoutSummary.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import classes from './CheckoutSummary.module.css';
+
 const CheckoutSummary = props => {
     console.log(props)
     return (
-        <div key={props.index}>
-             Q{props.index+1}: You answered: <span>{props.option.optValue}</span> Correct Answer: <span>{props.option.ans}</span>
+        <div key={props.index} className={classes.QuesBlock}>
+             Q{props.index+1}: You answered: <span className={props.option.optNum === 0 ? classes.Correct : classes.Wrong  }>{props.option.optValue}</span> Correct Answer: <span className={classes.Correct}>{props.option.ans}</span>
         </div>
     )
 }
